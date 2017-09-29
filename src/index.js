8f73e6e0e691015f5eb476add91e8c7c087cbbe7
ibadyulya <ivbadul@gmail.com>
2017-09-29 05:34:18 +0300

    20/22 passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function zeros(expression) {
-  // your solution
+module.exports = function zeros(expression) 
+{
+    var zerosCount = 0;
+    var factr = [];
+    var dblFactr = [];
+    var power = 0;
+    var splt1 = [];
+    var splt2 = [];
+
+    splt1 = expression.split('*');
+    for(var i = 0; i < splt1.length; i++)
+    {
+        splt2 = splt1[i].split('!');
+        (splt2.length == 2) ? factr.push(splt2[0]) :  dblFactr.push(splt2[0]);  
+    }
+    for(var i = 0; i < factr.length ;i++)
+    {
+        power = ~~getBaseLog(5, factr[i]);
+        while (power > 0) 
+        {
+            zerosCount += ~~(factr[i]/Math.pow(5,power));
+            power--;
+        }     
+    }
+    for(var i = 0; i < dblFactr.length ;i++)
+    {
+        if(dblFactr[i] % 2)
+        {
+            zerosCount += Math.ceil(parseInt(dblFactr[i]/5)/2);
+            zerosCount += Math.ceil(parseInt(dblFactr[i]/25)/2);
+        }
+        else
+        {  
+            zerosCount += ~~(dblFactr[i]/10);
+            zerosCount += ~~(dblFactr[i]/50);       
+        }
+    }
+
+    return zerosCount;
+}
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x); //logxY
 }
